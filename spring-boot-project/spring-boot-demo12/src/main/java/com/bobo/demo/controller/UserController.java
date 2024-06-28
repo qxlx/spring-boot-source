344982f15321276c9139948da9168c9f354c4f58
qxlx <qxlxi@qq.com>
2024-06-28 23:24:54 +0800

    0218更新

--- spring-boot-project/spring-boot-demo12/src/main/java/com/bobo/demo/controller/UserController.java
+++ spring-boot-project/spring-boot-demo12/src/main/java/com/bobo/demo/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.bobo.demo.controller;
 
-import com.bobo.demo.event.MyEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
